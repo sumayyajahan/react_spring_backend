04b8d94863b7f63e68dfb8a8cde93c8f833280eb
sumayyayellow <sumayyayellow@gmail.com>
2022-12-27 21:55:51 -0600

    first commit

---
+++ src/main/java/com/example/reactspring/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.reactspring.controller;
+
+import com.example.reactspring.model.Employee;
+import com.example.reactspring.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
